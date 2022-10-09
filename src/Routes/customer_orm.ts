4d47a799e61ac4efe8413f4ffde36a9e7edce2b3
GunPKate <gun@builk.com>
2022-10-09 22:27:32 +0700

    improve create customer API

--- src/Routes/customer_orm.ts
+++ src/Routes/customer_orm.ts
@@ -12,18 +12,33 @@ customer_orm.get('/customer_orm/get',(req:Request,res:Response)=>{
     })
 })
 
-customer_orm.post('/customer_orm/create',(req:Request,res:Response)=>{
+customer_orm.post('/customer_orm/create',async (req:Request,res:Response)=>{
     try {
-        Customer.insert({
-            firstName:"Gun",
-            lastName:"P",
-            email:"gun@p.com",
-            password:'1111'
+        // console.log(req.body)
+        let input = req.body
+        let  findresult = await Customer.find({
+           
         })
-        res.status(200).json({
-            resultcode:20000,
-            resultDescription: "data created"
-        })
+        // console.log(findresult);
+        
+        if(!findresult){
+            const data = Customer.insert({
+                firstName: input.firstName,
+                lastName:  input.lastName,
+                email:     input.email,
+                password:  input.password
+            })
+            res.status(200).json({
+                resultcode:20000,
+                resultDescription: "data created",
+                resultData: data
+            })
+        }else{
+            res.status(400).json({
+                resultcode:40000,
+                resultDescription: "duplicated data"
+            })
+        }
     } catch (error) {
         res.status(500).json({
             resultcode:50000,
